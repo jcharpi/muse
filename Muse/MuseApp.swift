1f4f93ede515e624167d03450a4df0c410a5e246
Josh Charpentier <charpentier.josh@gmail.com>
2024-12-31 13:05:03 -0600

    Initial Commit

---
+++ Muse/MuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseApp.swift
+//  Muse
+//
+//  Created by Josh Charpentier on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct MuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
